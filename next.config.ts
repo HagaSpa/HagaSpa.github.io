86c3880f656ebb6715be74f5defa1794639d029a
HagaSpa <justicesparrow13@gmail.com>
2025-06-29 15:25:12 +0900

    fix: basePath, assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/HagaSpa.github.io',
-  assetPrefix: '/HagaSpa.github.io/',
   trailingSlash: true,
   images: {
     unoptimized: true,
