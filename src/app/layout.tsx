83b767e84563216e950ad383081630dac183a1b6
HagaSpa <justicesparrow13@gmail.com>
2025-07-11 14:31:43 +0900

    fix:change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Skill Sheet",
   description: "HagaSpa's Skill Sheet",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
